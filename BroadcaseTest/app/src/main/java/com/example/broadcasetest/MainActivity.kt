404e6b8366ae2b63013aa16ee84895f5201ef535
zhouxiaofei <zhouxiaofei0001@gmail.com>
2019-12-07 13:10:29 +0800

    添加broadcasttest

--- BroadcaseTest/app/src/main/java/com/example/broadcasetest/MainActivity.kt
+++ BroadcaseTest/app/src/main/java/com/example/broadcasetest/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.broadcasetest
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Network
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var intentFilter: IntentFilter
+    lateinit var networkReceiver: NetworkChangeReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intentFilter = IntentFilter()
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
+        networkReceiver = NetworkChangeReceiver()
+        registerReceiver(networkReceiver, intentFilter)
+
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent("com.example.broadcasttest.MY_BROADCAST")
+            sendBroadcast(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(networkReceiver)
+        super.onDestroy()
+    }
+
+    class NetworkChangeReceiver: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            Toast.makeText(context,"receive message", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
---
+++ BroadcaseTest/app/src/main/java/com/example/broadcasetest/MyBroadcastReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.broadcasetest
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        Toast.makeText(context, "my broadcast", Toast.LENGTH_SHORT).show()
+    }
+}
